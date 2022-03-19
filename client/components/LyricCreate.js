1f53982dfb6c438e96ab6e762c9ab4e6f7907690
fayez-baig <coderninja16@gmail.com>
2022-03-19 17:23:56 +0530

    enhancing lyric create

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,35 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import { useMutation } from "@apollo/client";
-import { withRouter } from 'react-router-dom';
-import { ADD_LYRIC_TO_SONG } from '../queries/apolloQueries';
+import { useParams } from "react-router-dom";
+import { ADD_LYRIC_TO_SONG, GET_SONG } from "../queries/apolloQueries";
 
-const LyricCreate = ({songId, history}) => {
+const LyricCreate = ({ songId, history }) => {
+  const [content, setContent] = useState("");
+  const { id } = useParams();
+  const [addLyricToSongMutation] = useMutation(ADD_LYRIC_TO_SONG);
 
-    const [content, setContent] = useState('');
-    const [ addLyricToSongMutation] = useMutation(ADD_LYRIC_TO_SONG);
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+    addLyricToSongMutation({
+      variables: {
+        songId,
+        content,
+      },
+      refetchQueries: [{ query: GET_SONG, variables: { id } }],
+    }).then(() => setContent(""));
+    // .then(() => history.push("/"));
+  };
 
-        addLyricToSongMutation({ 
-            variables: { 
-                songId,
-                content
-            }
-        }).then(()=> setContent(''))
-        .then(() => history.push('/'));
-    }
-
-    return (
-        <form onSubmit={onSubmit}>
-            <label>Add a Lyric</label>
-            <input 
-                value={content}
-                onChange={event => setContent(event.target.value)}
-            />
-        </form>
-    );
+  return (
+    <form onSubmit={onSubmit}>
+      <label>Add a Lyric</label>
+      <input
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      />
+    </form>
+  );
 };
 
 export default LyricCreate;
